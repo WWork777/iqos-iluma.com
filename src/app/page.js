bec8b49ce89f86f726530add6da98eef3d783929
id0ntc4re <parsikovevgenij470@gmail.com>
2026-01-25 22:13:56 +0700

    modal off

--- src/app/page.js
+++ src/app/page.js
@@ -1,45 +1,48 @@
 import Image from "next/image";
-import './main.scss'
+import "./main.scss";
 import Hero from "../../components/Home/Hero/Hero";
 import Poster from "../../components/Home/Poster/Poster";
 import Preview from "../../components/Home/Preview/Preview";
 import About from "../../components/Home/About/About";
-import New from "../../components/Home/New/New";  
-import Exclusive from "../../components/Home/Exclusive/Exclusive";  
+import New from "../../components/Home/New/New";
+import Exclusive from "../../components/Home/Exclusive/Exclusive";
 import Reviews from "../../components/Home/Reviews/Reviews";
 import ModalManager from "../../components/ModalManager/ModalManager";
 
 export const metadata = {
   title: "IQOS Iluma купить в Москве | Стики Terea | iqos Iluma",
-  description: "Оригинальные устройства IQOS Iluma и стики Terea. Низкие цены, доставка по РФ. Гарантия качества и подлинности.",
+  description:
+    "Оригинальные устройства IQOS Iluma и стики Terea. Низкие цены, доставка по РФ. Гарантия качества и подлинности.",
   alternates: {
-    canonical: `https://iqos-iluma.com`
+    canonical: `https://iqos-iluma.com`,
   },
   openGraph: {
-      title: `IQOS Iluma купить в Москве | Стики Terea | iqos Iluma`,
-      description: `Оригинальные устройства IQOS Iluma и стики Terea. Низкие цены, доставка по РФ. Гарантия качества и подлинности.`,
-      url: `https://iqos-iluma.com`,
-      images: [
-          {
-              url: `/favicon/web-app-manifest-512x512`,
-              alt: `IqosIluma`,
-          },
-      ],
+    title: `IQOS Iluma купить в Москве | Стики Terea | iqos Iluma`,
+    description: `Оригинальные устройства IQOS Iluma и стики Terea. Низкие цены, доставка по РФ. Гарантия качества и подлинности.`,
+    url: `https://iqos-iluma.com`,
+    images: [
+      {
+        url: `/favicon/web-app-manifest-512x512`,
+        alt: `IqosIluma`,
+      },
+    ],
   },
 };
 
 export default function Home() {
   return (
     <>
-      <h1 className="hidden-h1">IQOS Iluma и стики Terea недорого и с доставкой по всей России</h1>
+      <h1 className="hidden-h1">
+        IQOS Iluma и стики Terea недорого и с доставкой по всей России
+      </h1>
       <Hero />
       <Poster />
       <Preview />
-      <Exclusive/>  
-      <New/>
+      <Exclusive />
+      <New />
       <About />
       <Reviews />
-      <ModalManager/>
+      {/* <ModalManager /> */}
     </>
   );
 }
