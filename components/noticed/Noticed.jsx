749c8fdc7c6fd06458dfd16c5e5d4fe8277dcce3
id0ntc4re <parsikovevgenij470@gmail.com>
2026-03-01 05:07:47 +0700

    qwe

---
+++ components/noticed/Noticed.jsx
@@ -0,0 +1,15 @@
+import "./styles.scss";
+
+export default function Noticed() {
+  return (
+    <section className="notice-bar">
+      <div className="notice-bar__container">
+        <p className="notice-bar__text">
+          Дистанционная розничная продажа никотиносодержащей продукции и
+          устройств для потребления никотиносодержащей продукции не
+          осуществляются
+        </p>
+      </div>
+    </section>
+  );
+}
